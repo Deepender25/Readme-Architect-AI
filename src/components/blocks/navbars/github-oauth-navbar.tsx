772d7e9fb9c4a7758024b130c4bdb096827f50ec
Deepender25 <yadavdeepender65@gmail.com>
2025-10-21 18:18:48 +0530

    Move user profile button left to center with dropdown menu

--- src/components/blocks/navbars/github-oauth-navbar.tsx
+++ src/components/blocks/navbars/github-oauth-navbar.tsx
@@ -191,7 +191,7 @@ export default function GitHubOAuthNavbar() {
               ) : (
                 <motion.div
                   key="user-menu"
-                  className="relative"
+                  className="relative -ml-20"
                   initial={{ opacity: 0, scale: 0.95 }}
                   animate={{ opacity: 1, scale: 1 }}
                   exit={{ opacity: 0, scale: 0.95 }}
